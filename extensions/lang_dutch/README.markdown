17442c454eb2bc02a8b25cf0d5e62900a3cb56a1
Ruben Van Assche <rubenvanassche@gmail.com>
2014-01-20 01:41:56 +0100

    Dutch language

---
+++ extensions/lang_dutch/README.markdown
@@ -0,0 +1,12 @@
+# Language: Dutch (Nederlands)
+
+Dutch translation of [Symphony](http://www.symphony-cms.com).  
+
+## Installation & Updating
+
+Information about [installing and updating extensions](http://symphony-cms.com/learn/tasks/view/install-an-extension/) can be found in the Symphony documentation at <http://symphony-cms.com/learn/>.
+
+## Notes
+
+I am not a professional translator, but did my best.
+Please let me know if you find any problems or want access.
--- workspace/data-sources/data.berichten.php
+++ workspace/data-sources/data.berichten.php
@@ -2,7 +2,7 @@
 
 	require_once(TOOLKIT . '/class.datasource.php');
 
-	Class datasourceberichten extends SectionDatasource{
+	Class datasourceberichten extends SectionDatasource {
 
 		public $dsParamROOTELEMENT = 'berichten';
 		public $dsParamORDER = 'desc';
@@ -19,34 +19,54 @@
 		public $dsParamINCLUDEDELEMENTS = array(
 				'system:date',
 				'titel',
-				'slagzin',
-				'achtergrond'
+				'slagzin'
 		);
 		
 
-		public function __construct($env=NULL, $process_params=true){
+		public function __construct($env=NULL, $process_params=true) {
 			parent::__construct($env, $process_params);
 			$this->_dependencies = array();
 		}
 
-		public function about(){
+		public function about() {
 			return array(
 				'name' => 'Berichten',
 				'author' => array(
 					'name' => 'ruben van assche',
-					'website' => 'http://scouts-zurenborg.be/nieuw',
+					'website' => 'http://localhost:8888/symphony',
 					'email' => 'rubenvanassche@gmail.com'),
-				'version' => 'Symphony 2.3.1',
-				'release-date' => '2013-09-14T17:19:06+00:00'
+				'version' => 'Symphony 2.3.5',
+				'release-date' => '2014-01-20T00:37:35+00:00'
 			);
 		}
 
-		public function getSource(){
+		public function getSource() {
 			return '14';
 		}
 
-		public function allowEditorToParse(){
+		public function allowEditorToParse() {
 			return true;
+		}
+
+		public function execute(array &$param_pool = null) {
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+
+			try{
+				$result = parent::execute($param_pool);
+			}
+			catch(FrontendPageNotFoundException $e){
+				// Work around. This ensures the 404 page is displayed and
+				// is not picked up by the default catch() statement below
+				FrontendPageNotFoundExceptionHandler::render($e);
+			}
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage() . ' on ' . $e->getLine() . ' of file ' . $e->getFile()));
+				return $result;
+			}
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			return $result;
 		}
 
 	}
